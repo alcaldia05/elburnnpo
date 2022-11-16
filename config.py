8466483b519c1c4bcd3dc1a7cd71a85a5e9ed2ed
alcaldia05 <117576633+alcaldia05@users.noreply.github.com>
2022-11-16 08:55:52 -0500

    Update config.py

--- config.py
+++ config.py
@@ -1,13 +1,13 @@
 import os
 import ProxyCloud
 
-BOT_TOKEN =  os.environ.get('bot_token','5777043254:AAGky5P7SMDNfhpET7rtxY8qAhi-mUwA_VI')
+BOT_TOKEN =  os.environ.get('bot_token','5313616769:AAEQmIm-gOYmil1xFQhx_tkLEWcCLy18qKA')
 API_ID =  os.environ.get('api_id','17232014')
 API_HASH = os.environ.get('api_hash','5eb747e0b34842c90a32af2df2364e32')
 SPLIT_FILE = 1024 * 1024 * int(os.environ.get('split_file','99'))
 ROOT_PATH = 'root/'
-ACCES_USERS = os.environ.get('tl_admin_user','toni8790').split(';')
-#ACCES_USERS = ('tl_admin_user','toni8790','Gavi_04Arias')
+ACCES_USERS = os.environ.get('tl_admin_user','nautaii').split(';')
+#ACCES_USERS = ('tl_admin_user','nautaii','Gavi_04Arias')
 #ACCES_USERS = os.environ.get(ACCES_USERS)
 PROXY = ProxyCloud.parse(os.environ.get('proxy_enc','socks5h://KIDHKDYDJHJHGIYJJHGGGDYJIDIDRKGILKGGKG'))
 
